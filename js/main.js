0d20551ad809693c40a7c625de4f8a5a98c3f643
MrDark <arhammurtaza@proton.me>
2024-10-21 00:08:51 +0500

    Add product section to main page, add POS page

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 20,
+        autoplay: true,
+        center: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        autoHeight: true,
+        nav: false,
+        dots: true,
+    });
+});
